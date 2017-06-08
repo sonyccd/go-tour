535e682d67970b6c51fba4113737830eeeaec37c
Brad Bazemore <bwbazemore@uga.edu>
2017-06-08 09:35:08 -0400

    added chan

---
+++ chan.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func ping(ch chan string){
+	for{
+		ch <- "ping"
+	}
+}
+
+func catcher(ch chan string){
+	for{
+		msg := <- ch
+		fmt.Println(msg)
+		time.Sleep(time.Second * 1)
+	}
+}
+
+func main(){
+	var ch chan string = make(chan string)
+
+	go ping(ch)
+	go catcher(ch)
+
+	var input string
+	fmt.Scanln(&input)
+}
