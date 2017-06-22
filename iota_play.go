760d1b53d52bceaed70bbaf62c89353388c9015f
Brad Bazemore <bwbazemore@uga.edu>
2017-06-22 11:02:06 -0400

    not an iota of printing

--- iota_play.go
+++ iota_play.go
@@ -12,10 +12,24 @@ const(
 	FlagMulticast
 )
 
+const (
+	_ = 1 << (10 * iota)
+	KiB
+	MiB
+	GiB
+	TiB
+	PiB
+	EiB
+	ZiB
+	YiB
+)
+
 func main(){
 	fmt.Printf("%08b\n", FlagUp)
 	fmt.Printf("%08b\n", FlagBroadcast)
 	fmt.Printf("%08b\n", FlagLoopback)
 	fmt.Printf("%08b\n", FlagPointToPoint)
 	fmt.Printf("%08b\n", FlagMulticast)
+	
+	fmt.Println(YiB) //how do I print this?
 }
